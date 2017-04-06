27730b4f90d3b7497ac2f82f2930011e8725a685
Stefan Andersson <stefan.andersson@acorntechnology.se>
2017-04-06 23:33:32 +0200

    * First bootstrap commit

---
+++ battlesnake-client.js
@@ -0,0 +1,31 @@
+var express = require('express'),
+  bodyParser = require('body-parser'),
+  app = express(),
+  port = process.env.PORT || 3000
+
+var jsonParser = bodyParser.json()
+
+app.listen(port);
+
+console.log('todo list RESTful API server started on: ' + port)
+
+app.route('/')
+   .get(function(req, res) {
+
+    res.json({command:"pong!"})
+
+})
+
+
+
+   app.post('/start', jsonParser, function(req, res) {
+
+       res.json({command:"pong!"})
+
+   })
+
+   app.post('/move', jsonParser, function(req, res) {
+
+       res.json({command:"pong!"})
+
+   })
---
+++ core/board.js
@@ -0,0 +1,60 @@
+var BoardCellType = {
+    Empty: 0,
+    Food: 1,
+    Me: 2,
+    Enemy: 3,
+    Dead: 4,
+    Wall: 5
+}
+
+function Board(myId, width, height, food, deadSnakes, snakes) {
+
+    var setBoardPositionType = function (coordinate, cellType) {
+        grid[coordinate.x, coordinate.y] = cellType;
+    }
+
+    var setPointArray = function (coordinates, boardCellType) {
+        if (coordinates == null) return
+
+        for (var point in coordinates) {
+            setBoardPositionType(point, boardCellType)
+        }
+    }
+
+    var grid = new Array(width, height)
+
+    for (var x = 0; x < width; X++) {
+        grid[x] = [];
+    }
+
+    setPointArray(food, BoardCellType.Food);
+
+    if (deadSnakes != null)
+        for (var deadSnake in deadSnakes)
+            setPointArray(deadSnake.Coordinates, BoardCellType.Dead);
+
+    for (var snake in snakes) {
+        var coords = snake.Coordinates;
+
+        var boardCellType = snake.Id == myId ? BoardCellType.Me : BoardCellType.Enemy;
+
+        setPointArray(coords, boardCellType);
+    }
+
+    this.GetCellType = function (x, y) {
+        if (x < 0 || x >= width || y < 0 || y >= height)
+            return BoardCellType.Wall;
+
+        return grid[x, y] || BoardCellType.Empty;
+    }
+
+    this.Peek(peekCoordinate, direction)
+    {
+        var peekX = peekCoordinate.X + directionOffset[direction].X;
+        var peekY = peekCoordinate.Y + directionOffset[direction].Y;
+
+        return GetCellType(peekX, peekY);
+    }
+}
+
+export BoardCellType, Board
